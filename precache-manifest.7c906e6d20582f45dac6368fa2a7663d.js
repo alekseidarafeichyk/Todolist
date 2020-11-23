7c460d628897d92339c098c6cc67703babf72d9d
alekseidarafeichyk <alekseidarafeichyk@gmail.com>
2020-11-23 15:13:10 +0300

    Updates

---
+++ precache-manifest.7c906e6d20582f45dac6368fa2a7663d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90ae15eaf224ee99c11f605f378675ec",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "9c337dbe8ca01ce0ed51",
+    "url": "/Todolist/static/css/main.212e44c4.chunk.css"
+  },
+  {
+    "revision": "284598324b341089abf6",
+    "url": "/Todolist/static/js/2.fc2907b5.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/Todolist/static/js/2.fc2907b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c337dbe8ca01ce0ed51",
+    "url": "/Todolist/static/js/main.cc7c6d54.chunk.js"
+  },
+  {
+    "revision": "0041d33a43d070a35743",
+    "url": "/Todolist/static/js/runtime-main.390fd722.js"
+  }
+]);
